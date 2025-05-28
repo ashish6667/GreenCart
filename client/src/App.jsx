ec6e10a670eab88f84256da634c21ef04f56df7a
ashish6667 <ay1060045@gmail.com>
2025-05-28 18:39:00 +0530

    Navbar completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
+  const location = useLocation();
+  const isSellerPath = location.pathname.includes("seller");
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      {/* Hide Navbar on seller paths */}
+      {!isSellerPath && <Navbar />}
 
-export default App
+      <div className={`${!isSellerPath ? "px-6 md:px-16 lg:px-24 xl:px-32" : ""}`}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
