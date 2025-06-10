a0c341cbb251fd23d50746f216baec6a2a285922
ashish6667 <ay1060045@gmail.com>
2025-06-10 18:39:23 +0530

    logout api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,7 +33,7 @@ export const register = async (req, res) =>{
                 return res.json({success: true, user: {email: user.email, name: user.name}})
     } catch (error) {
         console.log(error.message);
-        return res.json({success: false, message: error.message});   
+         res.json({success: false, message: error.message});   
     }
 }
 
@@ -70,6 +70,34 @@ export const login = async (req, res)=>{
                 
     } catch (error) {
         console.log(error.message);
-        return res.json({success: false, message: error.message});
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Check Auth : /api/user/is-auth
+export const isAuth = async (req, res)=>{
+    try {
+        const userId = req.userId;
+        const user = await User.findById(userId).select("-password")
+        return res.json({success: true, user})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Logout User : /api/user/logout
+
+export const logout = async (req, res)=>{
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        });
+        return res.json({success: true, message: "Logged out"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message});
     }
 }
