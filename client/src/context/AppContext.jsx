1a87eb45614060c1cefa9956a2b6a5e5f61a7c14
ashish6667 <ay1060045@gmail.com>
2025-06-18 18:07:14 +0530

    login and logout functionality

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -36,6 +36,19 @@ export const AppContextProvider = ({ children }) => {
     }
   }
 
+  // Fetch User Auth Status , User Data and Cart Items
+  const fetchUser = async ()=>{
+    try {
+      const {data} = await axios.post('/api/user/is-auth')
+      if(data.success){
+        setUser(data.user)
+        setCartItems(data.user.cartItems)
+      }
+    } catch (error) {
+       setUser(null)
+    }
+  }
+
   // Fetch All Products
   const fetchProducts = async () => {
    try {
@@ -114,6 +127,7 @@ export const AppContextProvider = ({ children }) => {
 
 
   useEffect(() => {
+    fetchUser()
     fetchSeller()
     fetchProducts()
   }, [])
