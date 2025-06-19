4857376bf8b9100ff84a7b0a483291d87b447aa1
ashish6667 <ay1060045@gmail.com>
2025-06-19 15:20:20 +0530

    Payment Gateway Integration

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,5 +1,6 @@
 import Order from "../models/Order.js";
 import Product from "../models/Product.js";
+import stripe from "stripe";
 
 
 // Placed Order COD : /api/order/cod
@@ -10,7 +11,7 @@ export const placeOrderCOD = async (req, res)=>{
             return res.json({success: false, message: "Invalid data"})
         }
         // Calculate Amount Using Items
-        let amount = await items.reduce(async (ActiveXObject, item)=>{
+        let amount = await items.reduce(async (acc, item)=>{
             const product = await Product.findById(item.product);
             return (await acc) + product.offerPrice * item.quantity;
         },0)
@@ -26,6 +27,76 @@ export const placeOrderCOD = async (req, res)=>{
             paymentType: "COD",
         });
         return res.json({success: true, message: "Order Placed Successfully"})
+    } catch (error) {
+        return res.json({ success: false, message: error.message});
+    }
+}
+
+// Placed Order Stripe : /api/order/stripe
+export const placeOrderStripe = async (req, res)=>{
+    try {
+        const {userId, items, address} = req.body;
+        const {origin} = req.headers;
+
+
+        if(!address || items.length === 0){
+            return res.json({success: false, message: "Invalid data"})
+        }
+        let productData = [];
+
+        // Calculate Amount Using Items
+        let amount = await items.reduce(async (acc, item)=>{
+            const product = await Product.findById(item.product);
+            productData.push({
+                name: product.name,
+                price: product.offerPrice,
+                quantity: item.quantity,
+            });
+            return (await acc) + product.offerPrice * item.quantity;
+        },0)
+
+        // Add Tax Charge (2%)
+        amount += Math.floor(amount * 0.02);
+
+     const order = await Order.create({
+            userId,
+            items,
+            amount,
+            address,
+            paymentType: "online",
+        });
+
+        // Stripe Gateway Initialize
+        const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
+
+        // create line items for stripe
+
+        const line_items =  productData.map((item)=>{
+            return {
+                price_data: {
+                    currency: "usd",
+                    product_data: {
+                        name: item.name,
+                    },
+                    unit_amount: Math.floor(item.price + item.price * 0.02) * 100
+                },
+                quantity: item.quantity,
+            }
+        })
+
+        // Create Checkout Session
+        const session = await stripeInstance.checkout.sessions.create({
+            line_items,
+            mode: "payment",
+            success_url: `${origin}/loader?next=my-orders`,
+            cancel_url: `${origin}/cart`,
+            metadata: {
+                orderId: order._id.toString(),
+                userId,
+            }
+        })
+
+        return res.json({success: true, url: session.url});
     } catch (error) {
         return res.json({ success: false, message: error.message});
     }
