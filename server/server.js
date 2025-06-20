4e2e9b26754155f0e477b327873e9c40aa3819ec
ashish6667 <ay1060045@gmail.com>
2025-06-20 11:37:20 +0530

    project completed

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://green-cart-eta-three.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
